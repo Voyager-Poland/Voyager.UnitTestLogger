ba944f14503731f898c76680f9ae0a943fa51816
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2023-03-17 21:13:12 +0100

    ikona i skrypt

---
+++ scripts/PublishNuget.ps1
@@ -0,0 +1,8 @@
+$version='1.0.0'
+dotnet build -c Release   /property:Version=$version
+dotnet pack -c Release /property:Version=$version
+
+$ostatniPakiet = (gci .\src\Voyager.HealthEndpoint\bin\Release\*.nupkg | select -last 1).Name
+$sciezka = ".\src\Voyager.UnitTestLogger\bin\Release\$ostatniPakiet"
+
+#dotnet nuget push "$sciezka" -s Voyager-Poland
